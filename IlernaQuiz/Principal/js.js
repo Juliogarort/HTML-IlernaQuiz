de25f3b7a34301cf6a555c52bc9fd1661ab40989
Juliogarort <145558985+Juliogarort@users.noreply.github.com>
2024-03-14 19:25:28 +0100

    Página Web en su estado final.

--- IlernaQuiz/Principal/js.js
+++ IlernaQuiz/Principal/js.js
@@ -1,34 +1,26 @@
 "use strict";
 
-// Función para validar la edad del usuario
-function validarEdad() {
-    // Preguntar al usuario su edad
-    let edad = prompt("Por favor, ingrese su edad:");
-
-    // Convertir la entrada a un número entero
-    edad = parseInt(edad);
-
-    // Verificar si la edad es válida y mayor o igual a 18
-    if (isNaN(edad) || edad < 18) {
-        // Si la edad no es válida o es menor a 18, redirigir a la página correspondiente según el rango de edad
-        if (edad >= 0 && edad <= 3) {
-            window.location.href = "https://www.dodot.es/";
-        } else if (edad >= 4 && edad <= 6) {
-            window.location.href = "https://www.pocoyo.com/";
-        } else if (edad >= 7 && edad <= 12) {
-            window.location.href = "https://www.harrypotterplatform934.com/";
-        } else if (edad >= 13 && edad <= 17) {
-            window.location.href = "https://www.lego.com/";
-        } else {
-            alert("Lo siento, debes ser mayor de edad para acceder a esta página.");
-            // Si la edad no es válida, volver a llamar a la función para que el usuario vuelva a ingresar su edad
-            validarEdad();
-        }
+// Función para verificar la edad del usuario
+function verificarEdad(edad) {
+    if (edad >= 0 && edad <= 3) {
+        window.location.href = "https://www.dodot.es/";
+    } else if (edad >= 4 && edad <= 6) {
+        window.location.href = "https://www.pocoyo.com";
+    } else if (edad >= 7 && edad <= 12) {
+        window.location.href = "https://www.harrypotter.com";
+    } else if (edad >= 13 && edad <= 17) {
+        window.location.href = "https://www.lego.com";
     } else {
-        // Si la edad es válida y mayor o igual a 18, mostrar un mensaje de bienvenida
-        alert("¡Bienvenido/a a nuestra página web!");
+        // Si la edad no está en ningún rango, permitir el acceso a la web y mostrar mensaje de bienvenida
+        alert("¡Bienvenido/a a nuestra web!");
     }
 }
 
-// Llamar a la función para validar la edad cuando se carga la página
-window.onload = validarEdad;
+// Pedir la edad al usuario
+let edadUsuario = prompt("Por favor, ingrese su edad:");
+
+// Convertir la entrada a un número entero
+let edad = parseInt(edadUsuario);
+
+// Verificar la edad y redireccionar si es necesario
+verificarEdad(edad);
